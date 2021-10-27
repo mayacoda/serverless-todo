6fcaed39c11b2b0bbe2b9de3566419af825d4c28
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2021-10-27 21:57:12 +0200

    add crud node server

---
+++ app.js
@@ -0,0 +1,70 @@
+const admin = require('firebase-admin')
+const express = require('express')
+const cors = require('cors')
+
+admin.initializeApp({
+ credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+ })})
+
+db = admin.firestore()
+
+const todosDB = db.collection('todos')
+
+const app = express()
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+app.use(cors({credentials: true, origin: true}))
+
+
+app.get('/', async (req, res) => {
+  let allTodos = await getAllTodos()
+  res.send(allTodos)
+})
+
+app.post('/', async (req, res) => {
+  const id = await addTodo(req.body)
+  await updateTodo(id, { id })
+  res.send({ id })
+})
+
+app.delete('/:todoId', async (req, res) => {
+  await deleteTodo(req.params.todoId)
+  res.send('OK')
+})
+
+app.put('/:todoId', async (req, res) => {
+  await updateTodo(req.params.todoId, req.body)
+  res.send('OK')
+})
+
+let port = parseInt(process.env.PORT) || 3000;
+app.listen(port)
+console.log('Server is listening on port ' + port)
+
+async function getAllTodos() {
+  const todosSnapshot = await todosDB.get()
+  return todosSnapshot.docs.map(todo => todo.data())
+}
+
+async function addTodo(todo) {
+  const res = await todosDB.add(todo)
+  return res.id
+}
+
+async function deleteTodo(todoId) {
+  await todosDB.doc(todoId).delete()
+}
+
+async function updateTodo(todoId, todo) {
+  await todosDB.doc(todoId).update(todo)
+}
--- index.ts
+++ index.ts
@@ -1,44 +1,84 @@
+import axios from 'axios';
+
+const loading = document.querySelector('#loading');
+const allTasks = document.querySelector('#tasks');
 const taskInput = document.querySelector('#new-task') as HTMLInputElement; // polje za unos nove stavke.
 const addButton = document.querySelector('#add-button'); // dugme za dodavanje nove stavke
 const incompleteTaskHolder = document.querySelector('#incomplete-tasks'); // lista stavki koje treba da se urade
 const completedTasksHolder = document.querySelector('#completed-tasks'); // lista gotovih stavki
 
-function addTask() {
-  // preskoči stavke koje su prazne
-  if (taskInput.value === '') return;
+const url = 'http://localhost:3000';
 
-  console.log('Add Task...');
-  // kreiranje novog elementa za stavku
-  const listItem = createNewTaskElement(taskInput.value);
 
-  // dodavanje stavke u listu stavki koje treba da se urade
-  incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
+function init() {
+  allTasks.classList.add('hidden');
 
-  taskInput.value = '';
+  axios.get(url).then(res => {
+    loading.classList.add('hidden');
+    allTasks.classList.remove('hidden');
+
+    res.data.forEach(todo => {
+      const listItem = createNewTaskElement(todo.text, todo.id, todo.completed)
+      if (todo.completed) {
+        completedTasksHolder.append(listItem)
+      } else {
+        incompleteTaskHolder.append(listItem)
+      }
+    });
+
+    Array.from(incompleteTaskHolder.children).forEach(child => {
+      bindTaskEvents(child, taskCompleted);
+    });
+
+    Array.from(completedTasksHolder.children).forEach(child => {
+      bindTaskEvents(child, taskIncomplete);
+    });
+
+    console.log('res', res);
+  }).catch(err => {
+    console.error(err);
+  });
 }
 
-// postavljanje funkcije za obradu klik događaja
+init();
+
+function addTask() {
+  const taskText = taskInput.value;
+  if (taskText === '') return;
+
+  console.log('Add Task...');
+
+  axios.post(url, {
+    text: taskText,
+    completed: false
+  }).then(res => {
+    const id = res.data.id;
+    const listItem = createNewTaskElement(taskText, id, false);
+
+    incompleteTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+
+    taskInput.value = '';
+
+  });
+}
+
 addButton.addEventListener('click', addTask);
 
 // kreiranje nove stavke
-function createNewTaskElement(taskString) {
+function createNewTaskElement(taskString, id, completed) {
   const listItem = document.createElement('li');
-  // checkbox za unos
   const checkBox = document.createElement('input'); //checkbox
-  // labela
   const label = document.createElement('label'); //label
-  // polje za unos teksta
   const editInput = document.createElement('input'); //text
-  // dugme za izmenu stavke
   const editButton = document.createElement('button'); //edit button
-  // dugme za brisanje stavke
   const deleteButton = document.createElement('button'); //delete button
 
   label.innerText = taskString;
 
-  // podešavanje elemenata
   checkBox.type = 'checkbox';
+  if (completed) checkBox.checked = true
+
   editInput.type = 'text';
 
   editButton.innerText = 'Izmeni'; //innerText encodes special characters, HTML does not.
@@ -46,16 +86,16 @@ function createNewTaskElement(taskString) {
   deleteButton.innerText = 'Obriši';
   deleteButton.className = 'delete';
 
-  // dodavanje dece u element za stavku
   listItem.appendChild(checkBox);
   listItem.appendChild(label);
   listItem.appendChild(editInput);
   listItem.appendChild(editButton);
   listItem.appendChild(deleteButton);
+
+  listItem.setAttribute('data-task-id', id);
   return listItem;
 }
 
-// Izmeni postojeću stavku
 function editTask() {
   console.log('Edit Task...');
   console.log('Change \'edit\' to \'save\'');
@@ -64,68 +104,72 @@ function editTask() {
 
   const editInput = listItem.querySelector('input[type=text]');
   const label = listItem.querySelector('label');
-  const containsClass = listItem.classList.contains('editMode');
-  // proveriti da li je stavka već u stanju izmene
-  if (containsClass) {
-    // zamena labele sa tekstom iz polja za unos
-    label.innerText = editInput.value;
+  const isSaving = listItem.classList.contains('editMode');
+  if (isSaving) {
+    const id = listItem.getAttribute('data-task-id');
+    const newValue = editInput.value;
+
+    axios.put(url + '/' + id, {
+      text: newValue,
+    }).then(res => {
+      label.innerText = newValue;
+    }).catch(console.error);
+
   } else {
     editInput.value = label.innerText;
   }
 
-  // uključivanje ili isključivanje stanja izmene
   listItem.classList.toggle('editMode');
 }
 
-// Brisanje stavke
 function deleteTask() {
   console.log('Delete Task...');
 
   const listItem = this.parentNode;
   const ul = listItem.parentNode;
-  // brisanje stavke iz liste u DOM-u
-  ul.removeChild(listItem);
+  const id = listItem.getAttribute('data-task-id')
+
+  axios.delete(url + '/' + id).then(() => {
+    ul.removeChild(listItem);
+  }).catch(console.error)
 }
 
-// Markiranje stavke kao završenu
 function taskCompleted() {
   console.log('Complete Task...');
 
-  // dodavanje stavke u listu gotovih stavki
   const listItem = this.parentNode;
-  completedTasksHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskIncomplete);
+  const id = listItem.getAttribute('data-task-id')
+
+  axios.put(url + '/' + id, {
+    completed: true
+  }).then(() => {
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+  }).catch(console.error)
+
 }
 
-// Markiranje stavke kao nezavršenu
 function taskIncomplete() {
   console.log('Incomplete Task...');
 
-  // dodavanje stavke u listu stavki koje treba da se urade
   const listItem = this.parentNode;
-  incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
+  const id = listItem.getAttribute('data-task-id')
+
+  axios.put(url + '/' + id, {
+    completed: false
+  }).then(() => {
+    incompleteTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+  }).catch(console.error)
 }
 
 function bindTaskEvents(taskListItem, checkBoxEventHandler) {
   console.log('bind list item events');
-  //select ListItems children
   const checkBox = taskListItem.querySelector('input[type=checkbox]');
   const editButton = taskListItem.querySelector('button.edit');
   const deleteButton = taskListItem.querySelector('button.delete');
 
-  //Bind editTask to edit button.
   editButton.onclick = editTask;
-  //Bind deleteTask to delete button.
   deleteButton.onclick = deleteTask;
-  //Bind taskCompleted to checkBoxEventHandler.
   checkBox.onchange = checkBoxEventHandler;
 }
-
-Array.from(incompleteTaskHolder.children).forEach(child => {
-  bindTaskEvents(child, taskCompleted);
-});
-
-Array.from(completedTasksHolder.children).forEach(child => {
-  bindTaskEvents(child, taskIncomplete);
-});
